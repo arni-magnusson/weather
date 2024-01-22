81e28b27bf0e00881663f232dfe642f7588e8f6f
Arni Magnusson <thisisarni@gmail.com>
2024-01-22 17:49:08 +1100

    Calculate UV index

--- model.R
+++ model.R
@@ -1,9 +1,20 @@
 ## Run analysis, write model results
 
-## Before:
-## After:
+## Before: coordinates.csv, uv.rds (data)
+## After:  uv_index.csv (model)
 
 library(TAF)
+source("utilities.R")  # lookup_uv
 
 mkdir("model")
 
+# Read data
+coordinates <- read.taf("data/coordinates.csv")
+uv <- readRDS("data/uv.rds")
+
+# Look up UV values for city coordinates
+uv.index <- sapply(coordinates$City, lookup_uv)
+uv.index <- xtab2taf(uv.index, "Month")
+
+# Write TAF table
+write.taf(uv.index, dir="model")
--- utilities.R
+++ utilities.R
@@ -22,16 +22,41 @@ import_uv_nasa <- function(month)
 
   # Read data
   path <- paste0("boot/data/uv/", sprintf("%02d", month), ".csv")
-  month <- read.csv(path, header=FALSE)
+  uv <- read.csv(path, header=FALSE)
 
   # Assign dimnames
   north <- seq(89.75, by=-0.5, length=360)
   east <- seq(-179.75, by=0.5, length=720)
-  dimnames(month) <- list(north, east)
+  dimnames(uv) <- list(north, east)
 
   # Rearrange data frame
-  month <- xtab2long(month, c("North", "East", "Index"))
-  month <- data.frame(month[1:2], Month=1L, month[3])
+  uv <- xtab2long(uv, c("North", "East", "Index"))
+  uv <- data.frame(uv[1:2], Month=as.integer(month), uv[3])
 
-  month
+  uv
+}
+
+################################################################################
+
+## Look up UV values for a location
+## Requires lookup (UV index values) and optionally cities (coordinates)
+## Example: lookup_uv("Reykjavik")         # using 'cities'
+##          or lookup_uv(64.1467, -21.94)  # using 'north' and 'south'
+
+lookup_uv <- function(north, east, lookup=uv, cities=coordinates)
+{
+  if(is.character(north))
+  {
+    east <- cities$East[cities$City == north]
+    north <- cities$North[cities$City == north]
+  }
+
+  north.vec <- sort(unique(lookup$North))
+  east.vec <- sort(unique(lookup$East))
+  n <- north.vec[which.min(abs(north.vec - north))]  # 60N->59.75N, 60S->60.25S
+  e <- east.vec[which.min(abs(east.vec - east))]  # 100W->100.25W, 100E->99.75E
+
+  index <- lookup$Index[lookup$North==n & lookup$East==e]
+  index <- setNames(index, month.abb)
+  index
 }
