4972cb342d2ef62703fc0fa58cafc4d8017e2290
Arni Magnusson <thisisarni@gmail.com>
2024-01-23 12:03:54 +1100

    Variables defined for all cities

--- data_overview.R
+++ data_overview.R
@@ -1,7 +1,7 @@
 ## Produce climate data overview, write TAF data tables
 
 ## Before: climate.rds (data)
-## After:  climate.csv (data)
+## After:  climate_overview.csv, climate_variables.dat (data)
 
 library(TAF)
 
@@ -14,9 +14,14 @@ var.list <- sapply(climate, names)
 vars <- sort(unique(unlist(var.list)))
 
 # Calculate climate data overview
-overview <- sapply(var.list, function(x) vars %in% x)
-rownames(overview) <- vars
-overview <- xtab2taf(t(overview), "City")
+climate.overview <- sapply(var.list, function(x) vars %in% x)
+rownames(climate.overview) <- vars
+climate.overview <- xtab2taf(t(climate.overview), "City")
+
+# Identify variables that are defined for all cities
+climate.variables <- sapply(climate.overview[-1], all)
+climate.variables <- names(climate.variables)[climate.variables]
 
 # Write table
-write.taf(overview, dir="data")
+write.taf(climate.overview, dir="data")
+write(climate.variables, "data/climate_variables.dat")
