b5ca3da600d67ef629af8fa7a8ece0f78b4dc38b
Arni Magnusson <thisisarni@gmail.com>
2024-01-23 14:41:57 +1100

    Show cities on map

--- report_uv.R
+++ report_uv.R
@@ -1,6 +1,7 @@
 ## Prepare UV index plot for report
 
-## Before: uv_cities.csv, uv_world_avg.csv, uv_world_max.csv (output)
+## Before: coordinates.csv (data),
+##         uv_cities.csv, uv_world_avg.csv, uv_world_max.csv (output)
 ## After:  uv_cities.png, uv_world_avg.png, uv_world_max.png (report)
 
 library(TAF)
@@ -10,18 +11,24 @@ mkdir("report")
 
 # Read data
 
+coordinates <- read.taf("data/coordinates.csv")
 uv.cities <- read.taf("output/uv_cities.csv", row.names=1)
 uv.world.avg <- read.taf("output/uv_world_avg.csv")
 uv.world.max <- read.taf("output/uv_world_max.csv")
 
 # Reorder cities
 uv.cities <- uv.cities[,order(sapply(uv.cities, mean))]
+coordinates <- coordinates[order(-abs(coordinates$North)),]
 
 # Rearrange world surface
 surface.avg <- xtabs(Index~East+North, uv.world.avg)
 surface.max <- xtabs(Index~East+North, uv.world.max)
 xcoords <- as.numeric(rownames(surface.avg))
 ycoords <- as.numeric(colnames(surface.avg))
+
+# Color palettes
+col.uv <- colorRampPalette(c("#ffffff", "#e0c0b0", "#d06000", "#804000"))(20)
+col.cities <- rep(palette()[c(3, 7, 2)], c(4, 3, 2))
 
 # Plot UV index annual cycle for cities
 taf.png("uv_cities_cycle")
@@ -31,23 +38,24 @@ dev.off()
 # Plot maximum UV index for cities
 taf.png("uv_cities_max")
 par(plt=c(0.18, 0.9475, 0.17, 0.8633))
-col <- rep(palette()[c(3, 7, 2)], c(4, 3, 2))
 barplot(sapply(uv.cities, max), horiz=TRUE, las=TRUE, xlab="Maximum UV index",
-        col=col)
+        col=col.cities)
 dev.off()
 
 # World map of maximum UV index
 taf.png("uv_world_avg")
-col <- colorRampPalette(c("#ffffff", "#e0c0b0", "#d06000", "#804000"))(20)
-image(xcoords, ycoords, surface.avg, ylim=c(-56, 72), col=col, xlab="", ylab="",
-      main="Average UV index")
+image(xcoords, ycoords, surface.avg, ylim=c(-56, 72), col=col.uv,
+      main="Average UV index",  xlab="", ylab="")
 map(add=TRUE)
+points(North~East, coordinates, pch=16, col=col.cities)
+points(North~East, coordinates)
 dev.off()
 
 # World map of maximum UV index
 taf.png("uv_world_max")
-col <- colorRampPalette(c("#ffffff", "#e0c0b0", "#d06000", "#804000"))(20)
-image(xcoords, ycoords, surface.max, ylim=c(-56, 72), col=col, xlab="", ylab="",
-      main="Maximum UV index")
+image(xcoords, ycoords, surface.max, ylim=c(-56, 72), col=col.uv,
+      main="Maximum UV index", xlab="", ylab="")
 map(add=TRUE)
+points(North~East, coordinates, pch=16, col=col.cities)
+points(North~East, coordinates)
 dev.off()
